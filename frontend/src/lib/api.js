ff77bf1fb5b6b941123239806d304ef4878395d8
parivartanbethechange2 <parivartanbethechange2@gmail.com>
2026-09-05 02:14:45 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-
 const BACKEND_URL = (process.env.REACT_APP_BACKEND_URL || "").replace(/\/$/, "");
 export const API = BACKEND_URL ? `${BACKEND_URL}/api` : "/api";
-
 export const api = axios.create({
   baseURL: API,
   withCredentials: true,
 });
-
-export const fmt = (n) => new Intl.NumberFormat("en-IN").format(n);
-export const inr = (n) => `₹${new Intl.NumberFormat("en-IN").format(n)}`;
+export const fmt = (n) =>
+  new Intl.NumberFormat("en-IN").format(n);
+export const inr = (n) =>
+  `₹${new Intl.NumberFormat("en-IN").format(n)}`;
